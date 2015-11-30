f191bc4ea618eac0fec27196eb57f76bb6e86cdd
Carlos Killpack <carlos@infinite.ai>
2015-11-29 19:36:20 -0700

    Do the sensible thing

--- src/geoip.py
+++ src/geoip.py
@@ -4,26 +4,6 @@ geoip-service
 ~~~~~~~~~~~~~
 
 A simple, free GeoIP REST endpoint.
-
-Copyright (c) 2015 Zulu Marketing
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
 """
 
 import os
@@ -47,7 +27,7 @@ application = app = Flask(__name__)
 
 DEBUG = True if os.environ.get("DEBUG") == "True" else False
 LOCALES = {"de", "en", "es", "fr", "ja", "pt-BR", "ru", "zh-CN"}
-GEOIP_DB = os.environ.get("GEOIP_DB")
+GEOIP_DB = os.environ.get("GEOIP_DB", "GeoIP2.mmdb")
 
 app.config.from_object(__name__)
 
